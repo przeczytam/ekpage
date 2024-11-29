04409d03b045fc82bd17adb3e70a0e848e9c9886
Piotr <piotr@example.com>
2024-11-29 16:36:57 +0100

    ver 1

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php require_once("./layout/header.php"); ?>
+
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Grzegorz
+                        <span class="text-primary">Brzęczyszczykiewicz</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                    Hauptstraße 39  · 92670 Windischeschenbach · 	Oberpfalz, Bayern · Deutschland · (09681) 123 45 67 ·
+                        <a href="mailto:grzegorz@email.com">grzegorz@email.com</a>
+                    </div>
+                    <p class="lead mb-5">Vertrauen Sie auf meine Motivation und Freude daran, in realen Projekten mitzuwirken und zum Erfolg Ihres Unternehmens beizutragen. Lassen Sie mich Ihr Team durch meine Begeisterung für die Betreuung komplexer IT-Lösungen und den direkten Anwenderkontakt bereichern.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <?php require_once("./layout/roadmap.php"); ?>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programmierung Sprachen & Werkzeuge</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                    
+                    </ul>
+                    <div class="subheading mb-3">Arbeitsablauf</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Netzwerke, TCP/IP, Netzwerktopologien, Routing, Switching, RAID, Backup-Strategien, Stromversorgung/USV
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Softwareentwicklung, Vorgehensmodelle, Zahlensysteme, Daten, Lasten- und Pflichtenheft, Datenbanksysteme, ER-Modelle, Testprozesse
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Internet of Things, MQTT-Protokoll, CPS, IoT-Protokolle
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Service-Management-Arten, Störungs- und Serviceanfragen, Kommunikationen, Monitoring
+                        </li>
+                    </ul>
+                    <div class="subheading mb-3">Sprachen</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                        <img src="./assets/img/poland.png" alt="Polnisch" style="margin-right: 20px;">&nbsp;<img src="./assets/img/germany.png" alt="Deutsch" style="margin-right: 20px;">&nbsp;<img src="./assets/img/united-kingdom.png" alt="Englisch">
+                        </li>
+
+
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Contact-->
+            <section class="resume-section" id="contact">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Kontaktieren Sie mich</h2>
+                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+        </div>
+<?php require_once("./layout/footer.php"); ?>
+</html>
---
+++ layout/roadmap.php
@@ -0,0 +1,30 @@
+<section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Über mich</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Der Weg ist das Ziel</h3>
+                            <div class="subheading mb-3">Deutsche Angestellte Akademie</div>
+                            <p>Ich absolviere derzeit eine Umschulung zum Fachinformatiker mit Schwerpunkt IT-Systemintegration. In meiner Ausbildung konnte ich bereits theoretische Kenntnisse in Netzwerktechnik, Datenbanken, Serveradministration und IT-Sicherheit erwerben, die ich in einem Praktikum vertiefe. Ein wichtiger Bestandteil ist zudem das betriebliche Projekt für die IHK-Abschlussprüfung, welches ich von der Planung bis zur Durchführung eigenständig bearbeite.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">seit Juni 2024</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Erfahrungen</h3>
+                            <div class="subheading mb-3">Mach das Beste aus dir, etwas Besseres kannst du nicht tun</div>
+                            <p>Meine bisherige berufliche Erfahrung umfasst sowohl die IT-Branche als auch die Kundenbetreuung. Mit der Umschulung habe ich nun die Möglichkeit, meine Leidenschaft für IT-Systemintegration in einem strukturierten Ausbildungsrahmen weiterzuentwickeln und mir eine langfristige berufliche Perspektive in diesem zukunftsorientierten Bereich zu schaffen. Besonders begeistert mich die Planung, Implementierung und Betreuung komplexer IT-Infrastrukturen sowie der direkte Kontakt zu Anwendern, um ihnen bei technischen Problemen zu helfen.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">bis May 2024</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Stärken</h3>
+                            <div class="subheading mb-3">Kreativer Teamplayer mit strukturierter Arbeitsweise</div>
+                            <p>Meine Stärken liegen in meiner strukturierten und gewissenhaften Arbeitsweise sowie in meiner Fähigkeit, mich schnell in neue Themengebiete einzuarbeiten. Unter Druck arbeite ich effizient und finde pragmatische Lösungen für auftretende Herausforderungen. Teamarbeit liegt mir sehr am Herzen, da ich weiß, wie wichtig eine gute Zusammenarbeit für den Erfolg eines Projekts ist. Mein Ziel ist es, mein Wissen weiter auszubauen und aktiv an realen IT-Projekten mitzuwirken, um wertvolle praktische Erfahrungen zu sammeln und zu einer erfolgreichen Projektumsetzung beizutragen</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">seit Februar 1973</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
